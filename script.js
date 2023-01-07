558e13d29d84f4dce469845c5e07c0d3fd242a33
developer-shop <114886950+developer-shop@users.noreply.github.com>
2023-01-06 21:21:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,18 @@
-const form = document.querySelector('form');
-eField = form.querySelector('.email'),
-eInput = eField.querySelector('input'),
-pField = form.querySelector('.password'),
-pInput = pField.querySelector('input');
+const likeBtn = document.querySelector(".like__btn");
+let likeIcon = document.querySelector("#icon"),
+  count = document.querySelector("#count");
+
+let clicked = false;
 
 
-form.onsubmit = (e)=>{
-    e.preventDefault();
-
-    (eInput.value == "") ? eField.classList.add("shake", "error") : checkEmail();
-    (pInput.value == "") ? pField.classList.add("shake", "error") : checkPassword();
-
-    setTimeout(() => {
-        eField.classList.remove('shake');
-        pField.classList.remove('shake');
-    }, 500);
-
-    eInput.onkeyup = ()=>{checkEmail();}
-    pInput.onkeyup = ()=>{checkPassword();}
-
-    function checkEmail(){
-        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; //pattern for validate email
-        if(!eInput.value.match(pattern)){
-            eField.classList.add("error");
-            eField.classList.remove("valid");
-            let errorTxt = eField.querySelector('.error-txt');
-            (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address" : errorTxt.innerText = "Email can't be blank"
-        }else{
-            eField.classList.remove("error");
-            eField.classList.add("valid");
-        }
-    }
-
-    function checkPassword(){
-        if(pInput.value == ""){
-            pField.classList.add('error');
-            pField.classList.remove('valid');
-        }else{
-            pField.classList.remove('error');
-            pField.classList.add('valid');
-        }
-    }
-
-    if(!eField.classList.contains('error') && !pField.classList.contains('error')){
-        window.location.href = form.getAttribute('action') //redirecting user to the specified url which is inside action attribute of form tag
-    }
-}
+likeBtn.addEventListener("click", () => {
+  if (!clicked) {
+    clicked = true;
+    likeIcon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+    count.textContent++;
+  } else {
+    clicked = false;
+    likeIcon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+    count.textContent--;
+  }
+});
